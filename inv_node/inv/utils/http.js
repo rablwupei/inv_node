4e531f073745cc7c72aebf857d0d78b7b737ca19
Wupei <rablwupei@gmail.com>
2016-09-24 23:53:53 +0800

    add iconv-lite

--- inv_node/inv/utils/http.js
+++ inv_node/inv/utils/http.js
@@ -2,17 +2,19 @@
  * Created by wupei on 16/9/24.
  */
 
-
 var request = require('request');
-request.debug = true;
+var iconv = require('iconv-lite');
+
 request({
-    url :'http://www.baidu.com',
+    url :'http://hq.sinajs.cn/list=sh601006',
     timeout : 30*1000,
     method : 'GET',
     // body : "abc",
     // gzip : true,
+    // encoding : 'GBK',
+    encoding: null,
 }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
     }
-    console.log("%s, %s, %s", error, response, body) // Show the HTML for the Google homepage.
+    console.log("%s, %s, %s", error, response, iconv.decode(body, 'GBK')) // Show the HTML for the Google homepage.
 })
